5af8b062f19d7f545da47a6085a90ae7b2bc9d64
unknown <pavithraa.doodleblue@gmail.com>
2022-03-27 12:42:33 +0530

    'day9_1'

---
+++ Day9_1/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+import Design from './Design.js';
+
+class App extends Component{
+  
+  render()
+  {
+  return (
+    <div>
+      <Design></Design>
+    </div>
+  )
+}
+}
+
+export default App;
