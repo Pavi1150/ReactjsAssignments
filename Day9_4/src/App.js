3bb998e737b74d3ff15e86949e36aad053b980ba
unknown <pavithraa.doodleblue@gmail.com>
2022-03-27 14:33:02 +0530

    'day9_4'

---
+++ Day9_4/src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import Accordion from './Accordion.js';
+
+class App extends Component{
+  
+  render()
+  {
+  return (
+    <div>
+      
+      <Accordion></Accordion>
+      
+    </div>
+  );
+}
+}
+
+export default App;
+ 
