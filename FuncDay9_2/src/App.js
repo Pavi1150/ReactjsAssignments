f865dff3b069e73a3a68b7204dc7b36627daa99a
unknown <pavithraa.doodleblue@gmail.com>
2022-04-01 00:30:28 +0530

    'day9iifunc'

---
+++ FuncDay9_2/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+import Countapp from './Countapp.js'
+
+class App extends Component {
+  render()
+  {
+  return (
+    <div>
+          <Countapp></Countapp>
+          
+    </div>
+  );
+}
+}
+
+export default App;
