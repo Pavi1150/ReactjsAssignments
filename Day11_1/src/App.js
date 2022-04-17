b9e000ebfd91c7f7c36de88a5c5856547aede1ce
unknown <pavithraa.doodleblue@gmail.com>
2022-04-18 00:58:36 +0530

    'day11_1'

---
+++ Day11_1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// import Coding from './Coding';
+import Menu1 from './Menu1.js';
+
+
+function App() {
+  return (
+    <div>
+      <Menu1/>
+      {/* <Coding/> */}
+    </div>
+  );
+}
+
+export default App;
