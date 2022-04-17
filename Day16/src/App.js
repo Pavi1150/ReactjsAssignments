88d688159c99841ca1b38d49fd430b9648a01b9c
unknown <pavithraa.doodleblue@gmail.com>
2022-04-18 02:31:01 +0530

    'day16'

---
+++ Day16/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Generate from './Generate.js'
+
+function App() {
+  return (
+    <div>
+     <Generate/>
+    </div>
+  );
+}
+
+export default App;
