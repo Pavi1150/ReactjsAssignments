8a7922eb17d7babb5fd3337075fa7c6b97e227a5
unknown <pavithraa.doodleblue@gmail.com>
2022-04-18 01:53:02 +0530

    'day12'

---
+++ Day12/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Tours from './Tours.js'
+
+function App() {
+  return (
+    <div >
+      <Tours/>
+    </div>
+  );
+}
+
+export default App;
