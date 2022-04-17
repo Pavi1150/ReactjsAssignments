1fcd4c87327ce81f25d31549bd8fe88c24619b46
unknown <pavithraa.doodleblue@gmail.com>
2022-04-18 01:43:20 +0530

    'day11_3'

---
+++ Day11_3/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Sidebar from './Sidebar.js';
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar/>
+      
+    </div>
+  );
+}
+
+export default App;
